eecdb974af35b488c73308ae09057a040fc00ab0
rohansharma0 <iamrohan29@gmail.com>
2021-05-14 01:27:18 +0530

    fix bugs and added custom font

--- app/src/main/java/com/iamrohan/covidtracker/MainActivity.java
+++ app/src/main/java/com/iamrohan/covidtracker/MainActivity.java
@@ -22,18 +22,22 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
+import com.facebook.shimmer.ShimmerFrameLayout;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     SharedPreferences preferences;
+
     private LinearLayout safetyLayout , countryChangeLayout;
     private ImageButton safetyBtn;
     private String countryName;
-    private ProgressBar progressBar;
-    ScrollView scrollView;
 
     private ImageView countryFlagImg;
 
@@ -55,22 +59,44 @@ public class MainActivity extends AppCompatActivity {
         todayDeaths,
         totalActive;
 
-    Boolean isCountryTab;
+
+    public final int countryTab = 0;
+    public final int globalTab = 1;
+
+
+    private int currentTab;
+
+    private ShimmerFrameLayout tvCasesShimmer,
+            tvRecoveredShimmer,
+            tvActiveShimmer,
+            tvTodayCasesShimmer,
+            tvDeathsShimmer,
+            tvTodayDeathsShimmer,
+            tvTodayRecoveredShimmer;
 
     @Override
     protected void onStart() {
         super.onStart();
-        isCountryTab = preferences.getBoolean("isActive" , false);
-        switchTab();
+        loadUI();
     }
 
     @Override
     protected void onResume() {
-
         super.onResume();
-        isCountryTab = preferences.getBoolean("isActive" , false);
-        switchTab();
+        loadUI();
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt("activeTabIndex" , currentTab);
+        editor.putString("countryName" , countryName);
+        editor.apply();
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,8 +111,8 @@ public class MainActivity extends AppCompatActivity {
         //getting countryCode from sharedPref
         countryName = preferences.getString("countryName" , "India");
 
-        // getting default tab
-        isCountryTab = preferences.getBoolean("isActive" , false);
+        // getting tabIndex;
+        currentTab = preferences.getInt("activeTabIndex" , globalTab);
 
         safetyLayout = findViewById(R.id.symptomsLayout);
         safetyBtn = findViewById(R.id.symptomsBtn);
@@ -104,11 +130,21 @@ public class MainActivity extends AppCompatActivity {
         tvCountryTab = findViewById(R.id.countryTab);
         tvGlobalTab = findViewById(R.id.globalTab);
 
-        progressBar = findViewById(R.id.progress_circular);
-        scrollView = findViewById(R.id.scrollView);
+        //Shimmer layout;
+        tvCasesShimmer = findViewById(R.id.tvConfirmedShimmer);
+        tvTodayCasesShimmer = findViewById(R.id.tvTodayConfirmedShimmer);
+        tvActiveShimmer = findViewById(R.id.tvActiveShimmer);
+        tvRecoveredShimmer = findViewById(R.id.tvRecoveredShimmer);
+        tvTodayRecoveredShimmer = findViewById(R.id.tvTodayRecoveredShimmer);
+        tvDeathsShimmer = findViewById(R.id.tvDeathsShimmer);
+        tvTodayDeathsShimmer = findViewById(R.id.tvTodayDeathsShimmer);
+
+        //load data and ui
+
+        loadUI();
 
 
-//        safety Layout and btn on click goto to Symptoms Activity
+        //safety Layout and btn on click goto to Symptoms Activity
 
         safetyLayout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -124,20 +160,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        tvCountryTab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                switchTab();
-            }
-        });
-
-        tvGlobalTab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                switchTab();
-            }
-        });
-
         countryChangeLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -145,11 +167,40 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        fetchData("https://corona.lmao.ninja/v3/covid-19/all/");
+        tvCountryTab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentTab = countryTab;
+
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putInt("activeTabIndex" , currentTab);
+                editor.apply();
+                loadUI();
+            }
+        });
+
+        tvGlobalTab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentTab = globalTab;
+
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putInt("activeTabIndex" , currentTab);
+                editor.apply();
+                loadUI();
+
+            }
+        });
+
     }
 
-    private void switchTab(){
-        if (isCountryTab ){
+    private void loadUI(){
+
+        countryName = preferences.getString("countryName" , "India");
+
+        loadData();
+
+        if(currentTab == countryTab){
             tvCountryTab.setTextColor(Color.parseColor("#142237"));
             tvCountryTab.setBackgroundResource(R.drawable.country_back);
             tvGlobalTab.setTextColor(Color.parseColor("#c8815b"));
@@ -162,15 +213,31 @@ public class MainActivity extends AppCompatActivity {
             tvCountryTab.setBackgroundColor(Color.parseColor("#00000000"));
 
         }
-        SharedPreferences.Editor editor = preferences.edit();
-        isCountryTab = !isCountryTab;
-        editor.putBoolean("isActive" ,isCountryTab);
-        editor.apply();
+
+
     }
 
-    private void fetchData(String url) {
+    private void loadData(){
+        if(currentTab == countryTab){
+            fetchData("https://corona.lmao.ninja/v3/covid-19/countries/" + countryName);
+        }else{
+            fetchData("https://corona.lmao.ninja/v3/covid-19/all/");
+        }
 
-        progressBar.setVisibility(View.VISIBLE);
+    }
+
+
+    private void fetchData(String url){
+
+
+        tvCasesShimmer.showShimmer(true);
+        tvTodayCasesShimmer.showShimmer(true);
+        tvActiveShimmer.showShimmer(true);
+        tvRecoveredShimmer.showShimmer(true);
+        tvTodayRecoveredShimmer.showShimmer(true);
+        tvDeathsShimmer.showShimmer(true);
+        tvTodayDeathsShimmer.showShimmer(true);
+
 
         StringRequest request = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
@@ -188,6 +255,28 @@ public class MainActivity extends AppCompatActivity {
                             todayDeaths = Double.parseDouble(jsonObject.getString("todayDeaths"));
                             totalActive = Double.parseDouble(jsonObject.getString("active"));
 
+
+
+
+                            if(currentTab == countryTab){
+
+                                JSONObject object = jsonObject.getJSONObject("countryInfo");
+                                String flagUrl = object.getString("flag");
+
+                                Glide.with(getApplicationContext()).load(flagUrl).placeholder(R.drawable.worldwide).circleCrop().into(countryFlagImg);
+                            }else{
+                                Glide.with(getApplicationContext()).load(R.drawable.worldwide).into(countryFlagImg);
+                            }
+
+                            tvCasesShimmer.hideShimmer();
+                            tvTodayCasesShimmer.hideShimmer();
+                            tvActiveShimmer.hideShimmer();
+                            tvRecoveredShimmer.hideShimmer();
+                            tvTodayRecoveredShimmer.hideShimmer();
+                            tvDeathsShimmer.hideShimmer();
+                            tvTodayDeathsShimmer.hideShimmer();
+
+
                             tvCases.setText(convert(totalCases));
                             tvTodayCases.setText("+ "+convert(todayCases));
                             tvRecovered.setText(convert(totalRecovered));
@@ -196,27 +285,15 @@ public class MainActivity extends AppCompatActivity {
                             tvDeaths.setText(convert(totalDeaths));
                             tvTodayDeaths.setText("+ "+convert(todayDeaths));
 
-
-                            progressBar.setVisibility(View.GONE);
-                            scrollView.setVisibility(View.VISIBLE);
-
-
-
-
                         } catch (JSONException e) {
                             e.printStackTrace();
-                            progressBar.setVisibility(View.GONE);
-                            scrollView.setVisibility(View.VISIBLE);
                         }
-
 
                     }
                 }, new Response.ErrorListener() {
 
             @Override
             public void onErrorResponse(VolleyError error) {
-                progressBar.setVisibility(View.GONE);
-                scrollView.setVisibility(View.VISIBLE);
                 Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
@@ -224,12 +301,11 @@ public class MainActivity extends AppCompatActivity {
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         requestQueue.add(request);
 
-
     }
 
     private String convert(Double n){
         if(n < 1000){
-            return String.format("%.2f",n);
+            return String.format("%.0f",n);
         }else if(n <100000){
             n = n / 1000;
             return String.format("%.2f K",n);
